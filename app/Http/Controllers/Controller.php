39e761fa87b3a75092abb56e9cabfdeb66e79749
joaomunizdev <jpedro747.jp@gmail.com>
2025-11-14 00:41:41 -0300

    chore: add swagger lib

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use OA;
+
+/**
+ * @OA\Info(
+ *      version="1.0.0",
+ *      x={
+ *          "logo": {
+ *              "url": "https://via.placeholder.com/190x90.png?text=L5-Swagger"
+ *          }
+ *      },
+ *      title="Checkout API Documentation",
+ *      description="Checkout API Swagger OpenApi",
+ *      @OA\Contact(
+ *          email="jpedro747.jp@gmail.com"
+ *      ),
+ * )
+ */
 abstract class Controller
 {
     //
